46c15f7e33bc549c69cb3b9f69065911d6729077
G3TSN1P3D <chava4flores@hotmail.com>
2022-07-26 13:17:48 -0400

    Added Doggie schema

--- server/models/Doggies.js
+++ server/models/Doggies.js
@@ -1,1 +1,33 @@
 const { Schema, model } = require('mongoose');
+
+const doggieSchema = new Schema(
+    {
+        dogName: {
+            type: String,
+            required: true,
+        },
+        ownersName: {
+            type: String,
+            required: true,
+        },
+        phoneNumber: {
+            type: Number,
+            required: true,
+            unique: true,
+            match: [/^\d{10}$/, "Must use a valid phone number"],
+        },
+        room: {
+            type: Number,
+            required: true,
+        },
+        crate: {
+            type: Number,
+            required: true,
+        },
+        checkIn: {
+            type: Date,
+            required: true,
+            default: Date.now,
+        }
+    }
+);
